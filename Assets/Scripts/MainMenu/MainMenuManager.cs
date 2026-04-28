e09de588ce72ab89fa6a353b3f6a78036a16b39e
Elise PIERRE <elise-pierre@outlook.com>
2026-04-28 16:40:58 +0800

    better story

--- Assets/Scripts/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -3,14 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public void PlayGame()
+    public void LoadStory()
     {
-        SceneManager.LoadScene("GameScene");
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
-        Debug.Log("Le jeu a ete ferme");
+        SceneManager.LoadScene("StoryScene");
     }
 }
