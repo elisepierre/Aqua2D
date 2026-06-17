b498bfcb02627653e3b9b04ec0b1bc8cff6771ee
Elise PIERRE <elise-pierre@outlook.com>
2026-06-17 17:08:25 +0800

    Update SirenObject.cs

--- Assets/Scripts/WAMGame/SirenObject.cs
+++ Assets/Scripts/WAMGame/SirenObject.cs
@@ -8,7 +8,7 @@ public class SirenObject : MonoBehaviour
     public float moveDistance = 2f;
 
     [Header("Sprites Sirène")]
-    public Sprite sadSprite; // Glisse le sprite de la sirène triste ici dans l'Inspecteur
+    public Sprite sadSprite;
 
     public int currentHoleIndex = -1;
     private Vector3 startPos;
@@ -23,10 +23,9 @@ public class SirenObject : MonoBehaviour
 
     void OnEnable()
     {
-        // On synchronise son temps sur la difficulté actuelle du manager
         if (WhackManager.Instance != null)
         {
-            activeTime = WhackManager.Instance.globalActiveTime + 1f; // On lui laisse un peu plus de temps sur le reef
+            activeTime = WhackManager.Instance.globalActiveTime + 1f;
         }
 
         startPos = transform.position;
@@ -37,10 +36,8 @@ public class SirenObject : MonoBehaviour
 
     IEnumerator LifeCycle()
     {
-        // Montée du trou
         yield return Move(startPos, targetPos);
 
-        // Attente hors du trou
         float timer = 0;
         while (timer < activeTime && !wasHit)
         {
@@ -48,7 +45,6 @@ public class SirenObject : MonoBehaviour
             yield return null;
         }
 
-        // Si elle n'a pas été touchée (Bravo, l'enfant a suivi les règles !)
         if (!wasHit)
         {
             yield return Move(targetPos, startPos);
@@ -64,7 +60,6 @@ public class SirenObject : MonoBehaviour
 
         Debug.Log("Aïe ! La sirène a été touchée !");
 
-        // JOUE LE SON TRISTE DE LA SIRÈNE ICI
         if (AudioManager.Instance != null)
         {
             AudioManager.Instance.PlaySFX(AudioManager.Instance.sirenSadClip);
@@ -80,15 +75,12 @@ public class SirenObject : MonoBehaviour
 
     IEnumerator SadBeforeGameOver()
     {
-        // Petit effet de secousse pour simuler le coup
         transform.position += new Vector3(0.05f, 0f, 0f);
         yield return new WaitForSeconds(0.05f);
         transform.position -= new Vector3(0.05f, 0f, 0f);
 
-        // On attend 0.4 seconde pour que l'enfant réalise sa bêtise en voyant la sirène triste
         yield return new WaitForSeconds(0.4f);
 
-        // Libération du trou et déclenchement du GameOver historique
         if (currentHoleIndex != -1) WhackManager.Instance.ReleaseHole(currentHoleIndex);
         WhackManager.Instance.GameOver();
     }
@@ -98,7 +90,7 @@ public class SirenObject : MonoBehaviour
         float t = 0;
         while (t < 1)
         {
-            if (wasHit) yield break; // Arrête le mouvement si on se fait taper dessus
+            if (wasHit) yield break;
             t += Time.deltaTime * moveSpeed;
             transform.position = Vector3.Lerp(from, to, t);
             yield return null;
