2ae466e356b7ac284f0bf6ab18d5b1f99a3dd9fa
Elise PIERRE <elise-pierre@outlook.com>
2026-06-17 16:58:33 +0800

    Update ScrollRepeatSeamless.cs

--- Assets/Scripts/EndlessGame/ScrollRepeatSeamless.cs
+++ Assets/Scripts/EndlessGame/ScrollRepeatSeamless.cs
@@ -1,9 +1,5 @@
 ﻿using UnityEngine;
 
-
-
-// À mettre sur l'objet qui contient votre fond d'eau
-
 public class ScrollRepeatSeamless : MonoBehaviour
 
 {
@@ -20,8 +16,6 @@ public class ScrollRepeatSeamless : MonoBehaviour
 
         material = GetComponent<Renderer>().material;
 
-        // Assurez-vous que l'image est bien en Repeat dans Unity (Asset Settings)
-
     }
 
 
@@ -30,15 +24,7 @@ public class ScrollRepeatSeamless : MonoBehaviour
 
     {
 
-        // On change l'offset vertical
-
         float offsetY = Time.time * scrollSpeed;
-
-
-
-        // Applique l'offset sur la texture principale (_MainTex)
-
-        // La texture va boucler d'elle-même grâce au raccordement parfait de l'image 37
 
         material.SetTextureOffset("_MainTex", new Vector2(0, offsetY));
 
